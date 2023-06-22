3321d97fd50e42df0dd0515d672bf55cc27fb475
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-06-22 23:26:54 +0200

    firebase temel

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: "AIzaSyDBnzXRChROmvv_EClLkHpOqsXG4VJuh-o",
+  authDomain: "video-c731f.firebaseapp.com",
+  projectId: "video-c731f",
+  storageBucket: "video-c731f.appspot.com",
+  messagingSenderId: "370746743977",
+  appId: "1:370746743977:web:087a245c71b024ebf95db8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export default app

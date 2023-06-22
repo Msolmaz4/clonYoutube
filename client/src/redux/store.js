96400892f8dcf8140f40ae7723c8515a618d5c3a
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-06-22 23:08:46 +0200

    Use with Redux-Persist WICHTIG WICGTIIIIIIIG

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,50 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore ,combineReducers } from '@reduxjs/toolkit'
+//combineReducers birlestirmeye temel olusturmak
 import userReducer from './userSlice'
+import videoReducer from "./videoSlice"
+
+//burada redux persist kullandik
+//Redux persist, redux’taki değişkenlerin, sayfa yenilendiğinde, değiştiğinde veya uygulama kapatıp tekrar açıldığında
+// tekrar eski haline dönmesini veya sıfırlanmasını engellemeye yarayan, bu değişkenleri localstorage da saklayan bir pakettir.
+//redux-toolkite bunu Use with Redux-Persist boyle aratirdim.
+//ornege bakarak yaptim rootReducer kendim zaydim cunkju baglantiyi saglamak icin daha guyek olur
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({user:userReducer , video : videoReducer})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer:persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store)
